a1e2de25d9c4e237038a4a0bfc95dccecef2fd1b
Apolo151 <abdallah.ibrahim5115@gmail.com>
2024-10-02 15:06:58 +0300

    update config

--- index.ts
+++ index.ts
@@ -1,27 +1,28 @@
 import  express  from "express";
 import { userRouter } from "./Routers/users";
 import {response,Request,NextFunction } from "express";
-const swaggerUI = require("swagger-ui-express");
-const YAML = require("yamljs");
-
 require("dotenv").config();
 
+
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
 const swaggerDocument = YAML.load("./swagger.yaml");
 
 const app = express();
 
+app.use(express.json());
+
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
-
-app.use(express.json());
 app.use("/api/registrations", userRouter);
-
 
 
 //health end point
 app.use("/api/health", (req, res) => {
     res.send({ status: "ok" });
 })
+
+// Error handling middleware
 app.use((err: any, req: any, res: any, next: NextFunction) => {
   console.error(err.stack); // Log the error stack for debugging
 
@@ -34,4 +35,7 @@ app.use((err: any, req: any, res: any, next: NextFunction) => {
     message: err.message || "Internal Server Error",
   });
 });
-app.listen(3000, () => console.log("Server started on port 3000"));
+
+// app
+app.listen(process.env.PORT, 
+  () => console.log(`Server started on port ${process.env.PORT}`));
